83e0d4eea6850c9eccb4245313991ca67e6147f6
Nandinijain23 <Nandinijain23>
2024-11-10 13:06:02 +0530

    commits

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'
-import { Route, Routes} from 'react-router-dom';
-import  './App.css'
+import App from './App.jsx';
+import  './App.css';
 import { HashRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter>
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
   
   </HashRouter>
 );
